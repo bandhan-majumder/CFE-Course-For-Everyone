7bc9209906d1fc850faa0ca5f62102ef8a616d42
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-09-26 11:47:13 +0530

    Fixed bug: imported useEffect

--- client/src/pages/learner/SignIn.jsx
+++ client/src/pages/learner/SignIn.jsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import LearnerOAuth from "@/components/LearnerOAuth";
 import Spinner from "@/components/ui/spinner";
