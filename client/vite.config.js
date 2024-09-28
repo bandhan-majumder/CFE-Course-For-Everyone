9e05460c9147af63b3ad0091189ca6b199bb54b6
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-09-28 17:54:54 +0530

    Updated config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       '/api': { // whenever there is a /api route, send the request to the localhost:3000 
         target: 'https://cfe-backend.onrender.com/',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
